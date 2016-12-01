b41e88f920421287a72d6acf74eacf57e4bc597e
Pétur Magnús <petur.petursson@outlook.com>
2016-12-01 15:39:31 +0000

    skil
    skil

---
+++ js.js
@@ -0,0 +1,347 @@
+ 
+  
+
+
+
+
+var model = {
+    
+
+Data:[  
+      {  
+         name:"David Bowie Heiðurstónleikar",
+         type:"Tónleikar",
+         date:"18-11-2016 Time 22:00:00",
+         artist:"Bitra ehf",
+         place:"Græni Hatturinn (Akureyri)",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9854.jpg",
+         GPSfitst:"65.681471",
+         GPSsecond:"18.089525"
+      },
+      {  
+         name:"Eftirlætis ljóðasöngvar",
+         type:"Tónleikar",
+         date:"19-11-2016 Time 13:30:00",
+         artist:"Hannesarholt ses",
+         place:"Hljóðberg (Hannesarholt)",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9732.jpg",
+         GPSfitst:"64.144259",
+         GPSsecond:"21.935552"
+      },
+      {  
+         name:"Shades of Reykjavík",
+         type:"Tónleikar",
+         date:"19-11-2016 Time 22:00:00",
+         artist:"Bitra ehf",
+         place:"Græni Hatturinn (Akureyri)",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9863.jpg",
+         GPSfitst:"65.681471",
+         GPSsecond:"18.089525"
+      },
+      {  
+         name:"Moses Hightower",
+         type:"Tónleikar",
+         date:"24-11-2016 Time 21:00:00",
+         artist:"Bitra ehf",
+         place:"Græni Hatturinn (Akureyri)",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9797.jpg",
+         GPSfitst:"65.681471",
+         GPSsecond:"18.089525"
+      },
+      {  
+         name:"Hjálmar",
+         type:"Tónleikar",
+         date:"25-11-2016 Time 22:00:00",
+         artist:"Bitra ehf",
+         place:"Græni Hatturinn (Akureyri)",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9714.jpg",
+         GPSfitst:"65.681471",
+         GPSsecond:"18.089525"
+      },
+      {  
+         name:"Ilmur af jólum",
+         type:"Aukatónleikar - A - svæði",
+         date:"26-11-2016 Time 16:00:00",
+         artist:"Hands up music ehf",
+         place:"Grafarvogskirkja",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9761.jpg",
+         GPSfitst:"64.132346",
+         GPSsecond:"21.800071"
+      },
+      {  
+         name:"Fjallabræður og vinir",
+         type:"Tónleikar",
+         date:"26-11-2016 Time 19:00:00",
+         artist:"Fjallabræður",
+         place:"Háskólabíó",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9816.jpg",
+         GPSfitst:"64.140506",
+         GPSsecond:"21.954583"
+      },
+      {  
+         name:"Ilmur af jólum",
+         type:"Jólatónleikar - A - svæði",
+         date:"26-11-2016 Time 20:00:00",
+         artist:"Hands up music ehf",
+         place:"Grafarvogskirkja",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9761.jpg",
+         GPSfitst:"64.132346",
+         GPSsecond:"21.800071"
+      },
+      {  
+         name:"LEONCIE",
+         type:"ALLRA SÍÐASTI SÉNS!",
+         date:"26-11-2016 Time 22:00:00",
+         artist:"Erðanúmúsik ehf",
+         place:"Hard Rock Café",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9823.jpg",
+       GPSfitst:"64.147201",
+       GPSsecond:"21.937125"
+      },
+      {  
+         name:"Fjallabræður og vinir",
+         type:"Aukatónleikar",
+         date:"26-11-2016 Time 22:30:00",
+         artist:"Fjallabræður",
+         place:"Háskólabíó",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9816.jpg",
+         GPSfitst:"64.140506",
+         GPSsecond:"21.954583"
+      },
+      {  
+         name:"Útgáfutónleikar Skafrenninganna",
+         type:"Tónleikar",
+         date:"27-11-2016 Time 20:00:00",
+         artist:"Ísaland slf.",
+         place:"Iðnó",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9827.jpg",
+         GPSfitst:"64.145921",
+         GPSsecond:"21.940023"
+      },
+      {  
+         name:"Hátíðartónleikar Eyþórs Inga",
+         type:"Sauðárkrókskirkja",
+         date:"27-11-2016 Time 20:00:00",
+         artist:"Hljómaland ehf",
+         place:"Sauðárkrókskirkja",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9864.jpg",
+         GPSfitst:"65.749903",
+         GPSsecond:"19.651029"
+      },
+      {  
+         name:"Þorvaldur Davíð og Skafrenningarnir",
+         type:"Tónleikar",
+         date:"30-11-2016 Time 20:30:00",
+         artist:"Ísaland slf.",
+         place:"Berg menningarhús (Dalvík)",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9828.jpg",
+         GPSfitst:"65.971272",
+         GPSsecond:"18.532208"
+      },
+      {  
+         name:"Hátíðartónleikar Eyþórs Inga",
+         type:"Hafnarkirkja Hornafirði",
+         date:"01-12-2016 Time 20:00:00",
+         artist:"Hljómaland ehf",
+         place:"Hafnarkirkja",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9864.jpg",
+         GPSfitst:"64.258426",
+         GPSsecond:"15.210367"
+      },
+      {  
+         name:"Þorvaldur Davíð og Skafrenningarnir",
+         type:"Tónleikar",
+         date:"01-12-2016 Time 21:00:00",
+         artist:"Bitra ehf",
+         place:"Græni Hatturinn (Akureyri)",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9826.jpg",
+         GPSfitst:"65.681471",
+         GPSsecond:"18.089525"
+      },
+      {  
+         name:"Fullveldispönk!",
+         type:"Pönksafn Íslands",
+         date:"01-12-2016 Time 22:00:00",
+         artist:"Erðanúmúsik ehf",
+         place:"Hard Rock Café",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9855.jpg",
+         GPSfitst:"64.147201",
+         GPSsecond:"21.937125"
+      },
+      {  
+         name:"Hátíðartónleikar Eyþórs Inga",
+         type:"Selfosskirkja",
+         date:"02-12-2016 Time 20:00:00",
+         artist:"Hljómaland ehf",
+         place:"Selfosskirkja",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9864.jpg",
+         GPSfitst:"63.938395",
+         GPSsecond:"21.009209"
+      },
+      {  
+         name:"Leppalúðar og létt jólatónlist",
+         type:"Tónleikar",
+         date:"02-12-2016 Time 22:00:00",
+         artist:"Bitra ehf",
+         place:"Græni Hatturinn (Akureyri)",
+         imgSrc:"https://d30qys758zh01z.cloudfront.net/images/medium/1.9796.jpg",
+         GPSfitst:"65.681471",
+         GPSsecond:"18.089525"
+      }
+   ]
+        
+};
+
+
+
+/*  Octopus */
+
+var octopus = {
+
+    init: function() {
+
+        // geri allt view reddy
+        eventsListView.init();
+        eventsListView.startSearch();
+        //eventsListView.filterByDate();
+    },
+
+     getData: function() {
+        return model.Data;
+    }
+};
+
+
+/* View */
+
+
+var eventsListView = {
+
+    init: function() {
+        // Geymi DOM element ef það þarf að nota þau
+        this.eventsListElem = document.getElementById('events-list');
+       // this.textElem = document.getElementById('textSearch');
+
+        // rendera vewið með öllum réttum values 
+        this.render();
+    },
+
+
+    startSearch:function(){
+      
+      var events = $('#events-list .eventContainer');
+  //get the search div
+  var search = $('#textSearch');
+  //make a cache for storing the element 
+  var cache = [];
+  //run through all the events 
+  events.each(function() {
+    cache.push({
+      element: this,
+      text: $(this).children('.eventName').text().trim().toLowerCase()
+    });
+  });
+  //filtering
+  function filter() {
+    //trim and make lowecase
+    var query = this.value.trim().toLowerCase();
+    //run throug all the events
+    cache.forEach(function(thing) {
+    //make the index variable 
+    var index = 0;
+    //get the index of the query i
+    index = thing.text.indexOf(query);
+    //set the style of the element according to whether or not a match was found
+    thing.element.style.display = index === -1 ? 'none' : '';
+  });
+  }
+  //hook the filter function to a keyup function
+  search.on('keyup', filter);
+},
+
+/*
+
+  reyndi að fá það til að virka en gekk ekki
+
+filterByDate:function () {
+  var lala = octopus.getData();
+  var events = $('#events-list .eventContainer');
+  var cache = [];
+  var time = lala.date;
+  events.each(function() {
+    var date = $(this).children('.eventTime').octopus.getData().time.trim().toLowerCase();
+    cache.push({
+      element: this,
+      date: fixedDate.getTime()
+    });
+  });
+  $("#dateButton").click(function() {
+    var fromDate = new Date($("#fromDate").val());
+    var toDate = new Date($("#toDate").val());
+    for (var i = 0; i < cache.length; i++) {
+      if (cache[i].date >= fromDate && cache[i].date <= toDate) {
+        cache[i].element.style.display = "";
+      }
+      else{
+        cache[i].element.style.display = "none";
+      }
+    }
+  });
+
+},*/
+
+    render: function() {
+
+      var Data = octopus.getData();
+
+
+      var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 5,
+      center: new google.maps.LatLng(64.870670, -18.440541),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker;
+      
+
+      
+
+
+
+        var events, elem, i, text, test;
+        
+        
+
+        
+        this.eventsListElem.innerHTML = '';
+
+      
+        for (i = 0; i < Data.length; i++) {
+            events = Data[i];
+            text = " <div class='eventContainer lala'> <p class='eventName'>"+events.name+"</p> <img src='"+events.imgSrc+"'> <br> <p>" + events.date +"</p> <br> <p>"+ events.place + "</p> </div>";  
+            elem = document.createElement('div');
+            elem.innerHTML = text;
+            
+            
+
+               marker = new google.maps.Marker({
+        position: new google.maps.LatLng(events.GPSfitst,-events.GPSsecond),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(Data[i].name + "<br> <img src='" +Data[i].imgSrc+"'>" + "<br>" +Data[i].place + "<br>" + Data[i].date);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+
+
+      this.eventsListElem.appendChild(elem);
+        }
+    }
+};
+// start
+octopus.init();
